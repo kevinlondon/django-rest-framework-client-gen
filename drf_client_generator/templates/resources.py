a03a480267f869240a3846be2363355bf786af42
Kevin London <kevin@wiredrive.com>
2015-02-11 14:07:42 -0800

    Cleaned up template, removed comments.

--- drf_client_generator/templates/resources.py
+++ drf_client_generator/templates/resources.py
@@ -4,28 +4,19 @@ import json
 RESOURCE_PATH =  "{{ resource_path }}"
 
 
-"""
 def create(resource=None, *args, **kwargs):
     print("POSTing {{ resource_name }}s.")
     resources.create(path=RESOURCE_PATH, model={{ resource_class }},
                      *args, **kwargs)
-"""
 
 
 def get(resource=None, *args, **kwargs):
     print("Getting {{ resource_name }}s.")
-    response =  resources.get(path=RESOURCE_PATH, resource=resource,
-                              *args, **kwargs)
-    print("Response: ", response.content)
-    body = json.loads(response.content)
-    instances = []
-    for result in body['results']:
-        instances.append(User(data=result))
-
-    return instances
+    response = resources.get(path=RESOURCE_PATH, resource=resource,
+                             *args, **kwargs)
+    return resources.parse(response, model={{ resource_class}})
 
 
-"""
 def update(resource=None, *args, **kwargs):
     print("Updating {{ resource_name }}s")
     resources.update(RESOURCE_PATH, resource=resource, *args, **kwargs)
@@ -34,7 +25,6 @@ def update(resource=None, *args, **kwargs):
 def delete(resource=None, *args, **kwargs):
     print("Deleting {{ resource_name }}s")
     resources.delete(RESOURCE_PATH, resource=resource, *args, **kwargs)
-"""
 
 
 class {{ resource_class }}(resources.Resource):
