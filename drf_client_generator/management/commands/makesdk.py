f603a0680e065f5de041712cb32674ad78e3d150
Kevin London <kevin@wiredrive.com>
2015-02-11 13:45:41 -0800

    Added basic skeleton to the project.

--- drf_client_generator/management/commands/makesdk.py
+++ drf_client_generator/management/commands/makesdk.py
@@ -15,30 +15,30 @@ def prep_environment():
 
 def get_resource_template():
     env = prep_environment()
-    return env.get_template("resource.py")
+    return env.get_template("resources.py")
 
 
 def make_resources(url_registry, repo_path):
     template = get_resource_template()
     for path, viewset, basename in url_registry:
         serializer = viewset.serializer_class
-        fpath = os.path.join(repo_path, "{0}s.py".format(basename))
+        fpath = os.path.join(repo_path, "{path}.py".format(path=path))
         with open(fpath, 'w') as fobj:
             context = {
                 "resource_class": serializer.Meta.model.__name__,
                 "resource_name": basename,
                 "fields": serializer().get_fields(),
+                "resource_path": path,
             }
             fobj.write(template.render(**context))
 
 
 class Command(BaseCommand):
-    args = '<package_name>'
     help = "This command makes an SDK for you."
 
     def handle(self, *args, **options):
         url = "gh:kevinlondon/cookiecutter-drf-client"
-        package_key = "Package Name"
+        package_key = "SDK Package Name"
         ctx = prompt_for_config(context={"cookiecutter": {package_key: "boilerplate_sdk"}})
         package_name = ctx[package_key]
         cookiecutter(url, no_input=True, extra_context={"repo_name": package_name})
---
+++ drf_client_generator/templates/resources.py
@@ -0,0 +1,43 @@
+from drf_client import resources, fields
+import json
+
+RESOURCE_PATH =  "{{ resource_path }}"
+
+
+"""
+def create(resource=None, *args, **kwargs):
+    print("POSTing {{ resource_name }}s.")
+    resources.create(path=RESOURCE_PATH, model={{ resource_class }},
+                     *args, **kwargs)
+"""
+
+
+def get(resource=None, *args, **kwargs):
+    print("Getting {{ resource_name }}s.")
+    response =  resources.get(path=RESOURCE_PATH, resource=resource,
+                              *args, **kwargs)
+    print("Response: ", response.content)
+    body = json.loads(response.content)
+    instances = []
+    for result in body['results']:
+        instances.append(User(data=result))
+
+    return instances
+
+
+"""
+def update(resource=None, *args, **kwargs):
+    print("Updating {{ resource_name }}s")
+    resources.update(RESOURCE_PATH, resource=resource, *args, **kwargs)
+
+
+def delete(resource=None, *args, **kwargs):
+    print("Deleting {{ resource_name }}s")
+    resources.delete(RESOURCE_PATH, resource=resource, *args, **kwargs)
+"""
+
+
+class {{ resource_class }}(resources.Resource):
+    {% for field, fieldtype in fields.iteritems() %}
+    {{ field }} = fields.Field()
+    {% endfor %}
