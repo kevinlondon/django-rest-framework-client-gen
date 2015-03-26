f51851043493672bc6d4bbe0758eb57f7093fcde
Kevin London <kevin@wiredrive.com>
2015-03-26 16:05:20 -0700

    Added field remapping class.

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,12 +2,28 @@ import os
 from mock import Mock
 import pytest
 from rest_framework.routers import SimpleRouter
-from drf_client_generator.models import ClientSDK
+from rest_framework import serializers
+
+from drf_client import fields
+from drf_client_generator.models import ClientSDK, Resource, FieldMapper
+from sampleapp.urls import router
+from sampleapp.models import FooSerializer
 
 
 @pytest.fixture
 def sdk():
     return ClientSDK(name="foo")
+
+
+@pytest.fixture
+def resource():
+    resource = Resource(sdk_path="foo", router_entry=router.registry[0])
+    return resource
+
+
+@pytest.fixture
+def field_mapper():
+    return FieldMapper(serializer_fields=None)
 
 
 class TestSDK:
@@ -25,3 +41,21 @@ class TestSDK:
     def test_router_parsing_ignores_other_things(self, sdk):
         sdk._urls = Mock(thing=object(), thing2=object())
         assert not sdk.routers
+
+
+class TestFieldMapping:
+
+    def test_field_remapper_will_remap_all_fields(self):
+        fields = FooSerializer().fields
+        mapper = FieldMapper(serializer_fields=fields)
+        remapped_fields = mapper.remap_all()
+        for field_name in fields:
+            assert field_name in remapped_fields
+
+    def test_datetime_field_maps_to_dt_field(self, field_mapper):
+        field = field_mapper.remap_field(serializers.DateTimeField())
+        assert isinstance(field, fields.DateTimeField)
+
+    def test_basic_field_maps_in_field_object(self, field_mapper):
+        field = field_mapper.remap_field(serializers.EmailField())
+        assert isinstance(field, fields.Field)
